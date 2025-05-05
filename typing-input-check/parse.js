135dc8c27e307b428b3c65ba23c08020262dd3e2
tekihei2317 <tekihei2317@gmail.com>
2025-05-05 13:09:28 +0900

    Google日本語入力のローマ字テーブルを追加

---
+++ typing-input-check/parse.js
@@ -0,0 +1,21 @@
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const filePath = path.join(__dirname, "romantable.txt");
+const content = await fs.readFile(filePath, { encoding: "utf-8" });
+
+console.log(
+  JSON.stringify(
+    content.split("\n").map((row) => {
+      const entry = row.split("\t");
+      return {
+        input: entry[0],
+        output: entry[1],
+        ...(entry[2] ? { nextInput: entry[2] } : {}),
+      };
+    })
+  )
+);
