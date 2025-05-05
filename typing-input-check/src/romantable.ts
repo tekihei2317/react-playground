5421673df13835e4317c890d3a2b8d422b969920
tekihei2317 <tekihei2317@gmail.com>
2025-05-05 21:18:15 +0900

    リファクタリング

--- typing-input-check/src/romantable.ts
+++ typing-input-check/src/romantable.ts
@@ -5,7 +5,6 @@ export type RomanTable = TableEntry[];
  * 文字列に該当するルールを検索する
  */
 function searchEntry(text: string): TableEntry | undefined {
-  console.log({ text });
   return romanTable.find((entry) => entry.input === text);
 }
 
@@ -48,21 +47,21 @@ export function convertBuffer(buffer: string): {
   const entry = searchEntry(buffer);
   const prefixEntries = searchEntriesByPrefix(buffer);
 
-  if (entry) {
-    if (prefixEntries.length <= 1) {
+  if (prefixEntries.length >= 2) {
+    // バッファに対する（将来的なものも含む）変換候補が複数ある場合は、変換を保留する
+    // 例: buffer=nのとき、nかnnかnaか分からないので保留する
+    return { buffer, output: "" };
+  } else {
+    if (entry) {
       // 変換ルールが1つしかない場合、そのルールを使って変換する
       return {
         output: entry.output,
         buffer: entry.nextInput ? entry.nextInput : "",
       };
     } else {
-      // バッファの文字で始まる変換ルールが複数ある場合は、変換を保留する（例: buffer=nのとき、nかnnかnaか分からない）
-      return { buffer, output: "" };
-    }
-  } else {
-    if (prefixEntries.length === 0) {
-      // bufferはこれから変換される可能性はないので、区切って変換を完了させる
+      // 変換ルールがない場合、バッファはこれから変換される可能性はないので、区切って変換を完了させる
       // |buffer|-1文字と、1文字の2つに分けて変換すればいいっぽい
+
       const left = buffer.slice(0, buffer.length - 1);
       const right = buffer.slice(buffer.length - 1);
 
@@ -72,9 +71,6 @@ export function convertBuffer(buffer: string): {
       // 例えば記号も入力する場合は、";a"などを";あ"に変換する必要があるので、ここで変換が必要。
       // 他にも、例えば"na"→"な"というルールがなければ、"na"をここで"んあ"に変換する必要がある。
       return { output: convertRoman(left), buffer: right };
-    } else {
-      // 同じく変換を保留する
-      return { buffer, output: "" };
     }
   }
 }
